167ed6556cb7b346a5efcc370cb8c3c6f067c44e
Yuchen Wu <cheneyuwu@gmail.com>
2021-10-30 02:00:31 -0400

    better plot gps

--- plot_gps.py
+++ plot_gps.py
@@ -19,8 +19,7 @@ class BagFileParser():
     try:
       self.conn = sqlite3.connect(bag_file)
     except Exception as e:
-      print(e)
-      print('could not connect')
+      print('Could not connect: ', e)
       raise Exception('could not connect')
 
     self.cursor = self.conn.cursor()
@@ -48,6 +47,70 @@ class BagFileParser():
     # Deserialise all and timestamp them
     return [(timestamp, deserialize_message(data, self.topic_msg_message[topic_name])) for timestamp, data in rows]
 
+def load_gps(bag_file, projection, start_gps_coord, start_xy_coord):
+
+  gps_poses = {"timestamp": [], "latitude": [], "longitude": [], "altitude": [], "cov": [], "x": [], "y": []}
+  gps_good_segments = {'x': [], 'y': []}
+
+  try:
+    parser = BagFileParser(bag_file)
+    messages = parser.get_bag_messages("/fix")
+  except Exception as e:
+    print("Could not open rosbag: {}".format(osp.dirname(bag_file)))
+    return gps_poses, gps_good_segments, True
+
+  print("Loaded ros bag: {}".format(osp.dirname(bag_file)))
+
+  # Collecting segements along a path that have good gps so we can plot a
+  # list of good segmants and ignore the bad data.
+  segment_ind = -1
+  prev_vert_bad_gps = True
+
+  num_large_covariance = 0
+
+  for gps_msg in messages:
+
+    # Check if covariance is good. Up to 0.1 is ok, I've chosen slightly
+    # stricter setting here.
+    covariance_is_large = gps_msg[1].position_covariance[0] >= 0.05
+    if covariance_is_large:
+      num_large_covariance += 1
+      prev_vert_bad_gps = True
+      continue
+    else:
+      # Start a new segment of good GPS data that can be plotted.
+      if prev_vert_bad_gps:
+        gps_good_segments["x"].append([])
+        gps_good_segments["y"].append([])
+        segment_ind += 1
+      prev_vert_bad_gps = False
+
+    # Projecting GPS so we can plot in meters.
+    x, y = projection(gps_msg[1].longitude, gps_msg[1].latitude)
+    # x, y = gps_msg[1].longitude, gps_msg[1].latitude
+
+    if not (len(start_gps_coord) and len(start_xy_coord)):
+      # Want to plot with the start of the path at (0, 0)
+      start_gps_coord.extend([gps_msg[1].latitude, gps_msg[1].longitude, gps_msg[1].altitude])
+      start_xy_coord.extend([x, y])
+
+    gps_poses["timestamp"].append(gps_msg[0])
+    gps_poses["latitude"].append(gps_msg[1].latitude - start_gps_coord[0])
+    gps_poses["longitude"].append(gps_msg[1].longitude - start_gps_coord[1])
+    gps_poses["altitude"].append(gps_msg[1].altitude - start_gps_coord[2])
+    gps_poses["x"].append(x - start_xy_coord[0])
+    gps_poses["y"].append(y - start_xy_coord[1])
+    gps_poses["cov"].append(gps_msg[1].position_covariance[0])
+
+    gps_good_segments["x"][segment_ind].append(x - start_xy_coord[0])
+    gps_good_segments["y"][segment_ind].append(y - start_xy_coord[1])
+
+  print("=> Number of segments:", len(gps_good_segments['x']))
+  if num_large_covariance > 0:
+    print("=> Large cov {}: {}, total: {}".format(osp.dirname(bag_file), num_large_covariance, len(messages)))
+
+  return gps_poses, gps_good_segments, False # bool(num_large_covariance > 0)
+
 
 def load_gps_poses(data_dir):
 
@@ -58,103 +121,46 @@ def load_gps_poses(data_dir):
   projection = Proj("+proj=etmerc +ellps=WGS84 +lat_0={0} +lon_0={1} +x_0=0 +y_0=0 +z_0={2} +k_0=1".format(
       proj_origin[0], proj_origin[1], proj_origin[2]))
 
-  start_gps_coord = None
-  start_xy_coord = None
+  start_gps_coord = []
+  start_xy_coord = []
 
-  gps_poses = []
-  bad_gps_runs = []
-  gps_good_segments = {"x": {}, "y": {}}
+  tot_gps_poses = []
+  tot_gps_good_segments = []
+  bad_runs = []
 
-  for i in range(len(rosbag_dirs)):
-    bag_file = '{0}/{1}/{1}_0.db3'.format(data_dir, rosbag_dirs[i])
+  for rosbag_dir in rosbag_dirs:
+    bag_file = '{0}/{1}/{1}_0.db3'.format(data_dir, rosbag_dir)
 
-    try:
-      parser = BagFileParser(bag_file)
-      messages = parser.get_bag_messages("/fix")
-    except Exception as e:
-      print("Could not open rosbag for run {}".format(rosbag_dirs[i]))
+
+    gps_poses, gps_good_segments, bad_run = load_gps(bag_file, projection, start_gps_coord, start_xy_coord)
+    if gps_poses is None:
       continue
 
-    print("Loaded ros bag: {}".format(rosbag_dirs[i]))
+    tot_gps_poses.append(gps_poses)
+    tot_gps_good_segments.append(gps_good_segments)
+    if bad_run:
+      bad_runs.append(rosbag_dir)
 
-    gps_poses.append({"timestamp": [], "latitude": [], "longitude": [], "altitude": [], "cov": [], "x": [], "y": []})
-    # Collecting segements along a path that have good gps so we can plot a
-    # list of good segmants and ignore the bad data.
-    gps_good_segments["x"][i] = [[]]
-    gps_good_segments["y"][i] = [[]]
-    segment_ind = 0
-    prev_vert_bad_gps = False
+  print("Bad GPS runs: {}".format(bad_runs))
 
-    num_large_covariance = 0
-
-    for j in range(len(messages)):
-
-      gps_msg = messages[j]
-
-      # Check if covariance is good. Up to 0.1 is ok, I've chosen slightly
-      # stricter setting here.
-      covariance_is_large = gps_msg[1].position_covariance[0] >= 0.05
-      if covariance_is_large:
-        num_large_covariance += 1
-        prev_vert_bad_gps = True
-      else:
-        # Start a new segment of good GPS data that can be plotted.
-        if prev_vert_bad_gps:
-          gps_good_segments["x"][i] += [[]]
-          gps_good_segments["y"][i] += [[]]
-          segment_ind += 1
-        prev_vert_bad_gps = False
-
-      # Projecting GPS so we can plot in meters.
-      x, y = projection(gps_msg[1].longitude, gps_msg[1].latitude)
-      # x, y = gps_msg[1].longitude, gps_msg[1].latitude
-
-      if start_gps_coord is None and start_xy_coord is None:
-        # Want to plot with the start of the path at (0, 0)
-        start_gps_coord = [gps_msg[1].latitude, gps_msg[1].longitude, gps_msg[1].altitude]
-        start_xy_coord = [x, y]
-
-      gps_poses[i]["timestamp"] += [gps_msg[0]]
-      gps_poses[i]["latitude"] += [gps_msg[1].latitude - start_gps_coord[0]]
-      gps_poses[i]["longitude"] += [gps_msg[1].longitude - start_gps_coord[1]]
-      gps_poses[i]["altitude"] += [gps_msg[1].altitude - start_gps_coord[2]]
-      gps_poses[i]["x"] += [x - start_xy_coord[0]]
-      gps_poses[i]["y"] += [y - start_xy_coord[1]]
-      gps_poses[i]["cov"] += [gps_msg[1].position_covariance[0]]
-
-      if not covariance_is_large:
-        gps_good_segments["x"][i][segment_ind] += [x - start_xy_coord[0]]
-        gps_good_segments["y"][i][segment_ind] += [y - start_xy_coord[1]]
-
-    if num_large_covariance > 0:
-      print("Large cov {}: {}, total: {}".format(rosbag_dirs[i], num_large_covariance, len(messages)))
-      bad_gps_runs.append(rosbag_dirs[i])
-
-  print("Bad GPS runs: {}".format(bad_gps_runs))
-
-  return gps_poses, gps_good_segments, rosbag_dirs, bad_gps_runs
+  return tot_gps_poses, tot_gps_good_segments, rosbag_dirs, bad_runs
 
 
-def plot_data(gps_poses, gps_good_segments, gps_runs, bad_gps_runs, plot_good_gps_only, data_dir):
+def plot_data(tot_gps_poses, tot_gps_good_segments, gps_runs, bad_gps_runs, data_dir):
 
   # plt.figure(figsize=(22, 15))
   plot_lines = []
   labels = []
 
-  for i in range(len(gps_poses)):
+  for i in range(len(tot_gps_poses)):
 
-    if (i in bad_gps_runs) and plot_good_gps_only:
-      continue
+    p = None
+    for j in range(len(tot_gps_good_segments[i]["x"])):
+      p = plt.plot(tot_gps_good_segments[i]["x"][j], tot_gps_good_segments[i]["y"][j], linewidth=2, color='C{}'.format(i))
 
-    if plot_good_gps_only:
-      p = None
-      for j in range(len(gps_good_segments["x"][i])):
-        p = plt.plot(gps_good_segments["x"][i][j], gps_good_segments["y"][i][j], linewidth=2, color='C{}'.format(i))
-    else:
-      p = plt.plot(gps_poses[i]["x"], gps_poses[i]["y"], linewidth=2, color='C{}'.format(i))
-
-    plot_lines.append(p[0])
-    labels.append(gps_runs[i])
+    if p is not None:
+      plot_lines.append(p[0])
+      labels.append(gps_runs[i])
 
   plt.ylabel('y (m)', fontsize=20, weight='bold')
   plt.xlabel('x (m)', fontsize=20, weight='bold')
@@ -163,7 +169,7 @@ def plot_data(gps_poses, gps_good_segments, gps_runs, bad_gps_runs, plot_good_gp
   plt.title('GPS ground truth, teach and repeat runs', fontsize=22, weight='bold')
   plt.legend(plot_lines, labels, fontsize=12)
   plt.show()
-  plt.savefig('{}/gps_paths.png'.format(data_dir), bbox_inches='tight', format='png')
+  # plt.savefig('{}/gps_paths.png'.format(data_dir), bbox_inches='tight', format='png')
   plt.close()
 
 
@@ -175,11 +181,9 @@ if __name__ == "__main__":
   # <rosbag name>/metadata.yaml
   # <rosbag name>/<rosbag name>_0.db3
   parser.add_argument('--path', default=os.getcwd(), type=str, help='path to vtr folder (default: os.getcwd())')
-  # Add argument if we want to only plot segements of the path with good GPS.
-  parser.add_argument('--only-good', dest='only_good', action='store_true')
 
   args = parser.parse_args()
 
-  gps_poses, gps_good_segments, gps_runs, bad_gps_runs = load_gps_poses(args.path)
+  tot_gps_poses, tot_gps_good_segments, gps_runs, bad_gps_runs = load_gps_poses(args.path)
 
-  plot_data(gps_poses, gps_good_segments, gps_runs, bad_gps_runs, args.only_good, args.path)
+  plot_data(tot_gps_poses, tot_gps_good_segments, gps_runs, bad_gps_runs, args.path)
